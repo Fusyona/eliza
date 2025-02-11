2ddd0f61270c7f44fd6d979b5e9c7e48c21853dc
Marcos <marcosmath44@gmail.com>
2025-02-10 21:17:59 -0500

    added validators for request data

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, HTTPException, Request
 import subprocess
+from fastapi.exceptions import RequestValidationError
 import docker
 import data_mapper
+from validators import RequestPayload
 import os
 import logging
 import json
+from starlette.responses import JSONResponse
 
 app = FastAPI()
 client = docker.from_env()
@@ -31,26 +34,34 @@ CHARACTER_DIR = "characters"
 os.makedirs(CHARACTER_DIR, exist_ok=True)
 
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    logger.error(f"❌ Validation error in {request.url}: {exc.errors()}")
+    return JSONResponse(
+        status_code=422,
+        content={"detail": exc.errors()},
+)
 @app.post("/run-container/")
-async def run_container(payload: dict):
+async def run_container(payload: RequestPayload):
+            
     try:
-        clients = payload.get("clients")
-        action = payload.get("action")
-        assistant_data = payload.get("data", {})
-        params = payload.get("params", {})
+        # clients = payload.clients
+        action = payload.action
+        assistant_data = payload.data
+        params = payload.params
 
-        if not clients or action != "start":
+        if not assistant_data.clients or action != "start":
             logger.warning(f"Invalid action: {action}")
-            raise HTTPException(status_code=400, detail="Invalid     client name or action")
+            raise HTTPException(status_code=400, detail="Invalid client name or action")
 
         # Extract secrets and other environment variables
-        env_vars = assistant_data.get("settings", {}).get("secrets", {})
-        character_name = assistant_data.get("name", "")
-        user_id = assistant_data.get("userId", "")
-        id = assistant_data.get("id", "")
+        env_vars = assistant_data.settings.secrets
+        character_name = assistant_data.name
+        user_id = assistant_data.userId
+        id = assistant_data.id
+        print(assistant_data.dict())
         
-        
-        character_data = data_mapper.get_character(assistant_data)
+        character_data = data_mapper.get_character(assistant_data.dict())
         
         # Save character data as JSON
         user_folder = os.path.join(CHARACTER_DIR, f"{user_id}")
@@ -61,7 +72,7 @@ async def run_container(payload: dict):
         logger.info(f"✅ Character '{character_name}' (ID: {character_name}) saved successfully")
         
         # Get enviroment variables
-        env = data_mapper.get_env(assistant_data)
+        env = data_mapper.get_env(assistant_data.dict())
 
         # Construct the environment variable string
         env_string = " ".join([f'{key}="{value}"' for key, value in env.items()])
